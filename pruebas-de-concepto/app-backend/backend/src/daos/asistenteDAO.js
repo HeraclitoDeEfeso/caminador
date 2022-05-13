d5c86ca53c6ff0baa7d13db901ec83d87657f73b
Cintia Capece <cintiflor@gmail.com>
2022-05-12 21:10:33 -0300

    Refactoriza promises de DAOS

--- pruebas-de-concepto/app-backend/backend/src/daos/asistenteDAO.js
+++ pruebas-de-concepto/app-backend/backend/src/daos/asistenteDAO.js
@@ -2,80 +2,29 @@ const Asistente = require('../models/asistente');
 
 class AsistenteDAO{
     static save(asistente){
-        return new Promise((resolve, reject) => {
-            Asistente.create(asistente, (err, asistenteStored) => {
-                if (err || !asistenteStored){
-                    reject({message: "no pudo guardarse el asistente"});
-                } else {
-                    asistente._id = asistenteStored._id;
-                    resolve(asistente);
-                }
-            });
-        })
+        return Asistente.create(asistente);
     }
 
     static fetch(id){
-        return new Promise((resolve, reject) => {
-            Asistente.findById(id).exec((err, asistente) => {
-                if (err || !asistente){
-                    reject ({message: "No pudo encontrarse el asistente"});
-                } else {
-                    resolve(asistente);
-                }
-            })
-        })
+        return Asistente.findById(id);
     }
 
     static find(filter, pagination){
-        return new Promise((resolve, reject) => {
-            Asistente.find(filter).limit(pagination.limit).skip(pagination.offset).exec((err, asistentes) => {
-                if (err || !asistentes){
-                    reject({message: "no se pudo realizar la busqueda"});
-                }else{
-                    resolve(asistentes);                    
-                }
-            })
-        })
+        return Asistente.find(filter).limit(pagination.limit).skip(pagination.offset);
     }
 
     static count(filter){
-        return new Promise((resolve, reject) => {
-            Asistente.count(filter).exec((err, total) => {
-                if (err){
-                    reject({message: "no se pudo realizar la busqueda"});
-                }else{
-                    resolve(total);                    
-                }
-            })
-        })
+        return Asistente.count(filter);
     }
 
     static update(id, asistente) {
 		const {_id, ...data} = asistente._doc;
 		let dtoUpdate = {$set:data}
-        return new Promise((resolve, reject) => {
-            Asistente.findByIdAndUpdate(id, dtoUpdate).exec((err, asistente2) => {
-                if (err || !asistente2){
-                    console.log(err);
-                    reject({message: "error interno"});
-                } else {
-                    resolve(asistente2);
-                }
-            })
-        })
+        return Asistente.findByIdAndUpdate(id, dtoUpdate);
     }
 
     static delete(id){
-        return new Promise((resolve, reject) => {
-            
-            Asistente.findByIdAndRemove(id).exec((err, deleted) => {
-                if (err || !deleted){
-                    reject({message: "no se puede borrar el asistente"});
-                } else {
-                    resolve({id:id});
-                }
-            })
-        })
+        return Asistente.findByIdAndRemove(id);
     }
 }
 
--- pruebas-de-concepto/app-backend/backend/src/daos/pacienteDAO.js
+++ pruebas-de-concepto/app-backend/backend/src/daos/pacienteDAO.js
@@ -2,80 +2,29 @@ const Paciente = require('../models/paciente');
 
 class PacienteDAO{
     static save(paciente){
-        return new Promise((resolve, reject) => {
-            Paciente.create(paciente, (err, pacienteStored) => {
-                if (err || !pacienteStored){
-                    reject({message: "no pudo guardarse el paciente"});
-                } else {
-                    paciente._id = pacienteStored._id;
-                    resolve(paciente);
-                }
-            });
-        })
+        return Paciente.create(paciente);
     }
 
     static fetch(id){
-        return new Promise((resolve, reject) => {
-            Paciente.findById(id).exec((err, paciente) => {
-                if (err || !paciente){
-                    reject ({message: "No pudo encontrarse el paciente"});
-                } else {
-                    resolve(paciente);
-                }
-            })
-        })
+        return Paciente.findById(id);
     }
 
     static find(filter, pagination){
-        return new Promise((resolve, reject) => {
-            Paciente.find(filter).limit(pagination.limit).skip(pagination.offset).exec((err, pacientes) => {
-                if (err || !pacientes){
-                    reject({message: "no se pudo realizar la busqueda"});
-                }else{
-                    resolve(pacientes);                    
-                }
-            })
-        })
+        return Paciente.find(filter).limit(pagination.limit).skip(pagination.offset);
     }
 
     static count(filter){
-        return new Promise((resolve, reject) => {
-            Paciente.count(filter).exec((err, total) => {
-                if (err){
-                    reject({message: "no se pudo realizar la busqueda"});
-                }else{
-                    resolve(total);                    
-                }
-            })
-        })
+        return Paciente.count(filter);
     }
 
     static update(id, paciente) {
 		const {_id, ...data} = paciente._doc;
 		let dtoUpdate = {$set:data}
-        return new Promise((resolve, reject) => {
-            Paciente.findByIdAndUpdate(id, dtoUpdate).exec((err, paciente2) => {
-                if (err || !paciente2){
-                    console.log(err);
-                    reject({message: "error interno"});
-                } else {
-                    resolve(paciente2);
-                }
-            })
-        })
+        return Paciente.findByIdAndUpdate(id, dtoUpdate);
     }
 
     static delete(id){
-        return new Promise((resolve, reject) => {
-            
-            Paciente.findByIdAndRemove(id).exec((err, deleted) => {
-                if (err || !deleted){
-                    reject({message: "no se puede borrar el paciente"});
-                } else {
-                    resolve({id:id});
-                }
-            })
-        })
+        return Paciente.findByIdAndRemove(id);
     }
 }
 
