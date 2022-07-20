bedfcea46e11d9bf8ac6cad743c67bace41fb326
Alejandro Araneda <eloscurodeefeso@gmail.com>
2022-07-19 23:19:36 -0300

    App & firmware: final

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,12 +2,13 @@
 #include <string.h>
 #include "Fsm.h"
 
-#define motorPin    6
-#define ledPin      12
-#define hardStopPin 4
-#define potePin     A0
-#define MAX_STEPS   10
-#define STEP_STRING_SIZE 8
+#define DEBUG             if(0)
+#define motorPin          6
+#define ledPin            12
+#define hardStopPin       4
+#define potePin           A0
+#define MAX_STEPS         10
+#define STEP_STRING_SIZE  8
 
 enum Events {
   EVENT_BEGIN_PROGRAM,
@@ -20,7 +21,9 @@ enum Events {
   EVENT_CONTINUE,
   EVENT_ERROR,
   EVENT_NULL,
-  EVENT_COMPLETED
+  EVENT_COMPLETED,
+  EVENT_HARD_STOP,
+  EVENT_UNLOCK
 };
 
 struct Step {
@@ -29,69 +32,73 @@ struct Step {
 };
 
 struct Step program[MAX_STEPS] = {0};
-State state_idle(&on_state_idle, NULL, NULL);
-State state_getset(&on_state_getset, NULL, NULL);
-State state_config(&on_state_config, NULL, NULL);
-State state_ready(&on_state_ready, NULL, NULL);
-State state_run(&on_state_run, NULL, NULL);
-State state_pause(&on_state_pause, NULL, NULL);
+
+State state_idle      (&on_state_idle,      NULL, NULL);
+State state_getset    (&on_state_getset,    NULL, NULL);
+State state_config    (&on_state_config,    NULL, NULL);
+State state_ready     (&on_state_ready,     NULL, NULL);
+State state_run       (&on_state_run,       NULL, NULL);
+State state_pause     (&on_state_pause,     NULL, NULL);
+State state_hard_stop (&on_state_hard_stop, NULL, NULL);
+
 Fsm fsm(&state_idle);
 
 int velocidad = 0;
 int pote = 0;
 int current_step = 0;
+
 unsigned long stop_time = 0;
 unsigned long remain_time = 0;
-enum Events event = EVENT_NULL;
-enum Events event_prev = EVENT_NULL;
+
+volatile enum Events event = EVENT_NULL;
 
 void setup() {
-  pinMode(motorPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
-  pinMode(hardStopPin, INPUT);
+
+  pinMode(motorPin,     OUTPUT);
+  pinMode(ledPin,       OUTPUT);
+  pinMode(hardStopPin,  INPUT);
+  
   Serial.begin(9600);
   while (!Serial);
-  fsm.add_transition(&state_idle, &state_getset, EVENT_BEGIN_PROGRAM, NULL);
-  fsm.add_transition(&state_idle, &state_config, EVENT_BEGIN_CONFIG, NULL);
-  fsm.add_transition(&state_getset, &state_idle, EVENT_DISCARD, NULL);
-  fsm.add_transition(&state_getset, &state_ready, EVENT_END_PROGRAM, NULL);
-  fsm.add_transition(&state_ready, &state_run, EVENT_RUN, NULL);
-  fsm.add_transition(&state_ready, &state_getset, EVENT_BEGIN_PROGRAM, NULL);
-  fsm.add_transition(&state_run, &state_ready, EVENT_COMPLETED, NULL);
-  fsm.add_transition(&state_run, &state_pause, EVENT_PAUSE, NULL);
-  fsm.add_transition(&state_run, &state_run, EVENT_NULL, NULL);
-  fsm.add_transition(&state_pause, &state_run, EVENT_CONTINUE, NULL);
-  fsm.add_transition(&state_config, &state_idle, EVENT_END_CONFIG, NULL);
-  //Serial.println("Setup ready.");
+  
+  fsm.add_transition(&state_idle,       &state_getset,    EVENT_BEGIN_PROGRAM,  NULL);
+  fsm.add_transition(&state_idle,       &state_config,    EVENT_BEGIN_CONFIG,   NULL);
+  fsm.add_transition(&state_getset,     &state_idle,      EVENT_DISCARD,        NULL);
+  fsm.add_transition(&state_getset,     &state_ready,     EVENT_END_PROGRAM,    NULL);
+  fsm.add_transition(&state_ready,      &state_run,       EVENT_RUN,            NULL);
+  fsm.add_transition(&state_ready,      &state_getset,    EVENT_BEGIN_PROGRAM,  NULL);
+  fsm.add_transition(&state_ready,      &state_idle,      EVENT_DISCARD,        NULL);
+  fsm.add_transition(&state_run,        &state_idle,      EVENT_COMPLETED,      NULL);
+  fsm.add_transition(&state_run,        &state_idle,      EVENT_DISCARD,        NULL);
+  fsm.add_transition(&state_run,        &state_pause,     EVENT_PAUSE,          NULL);
+  fsm.add_transition(&state_run,        &state_run,       EVENT_NULL,           NULL);
+  fsm.add_transition(&state_run,        &state_hard_stop, EVENT_HARD_STOP,      NULL);
+  fsm.add_transition(&state_hard_stop,  &state_idle,      EVENT_UNLOCK,         NULL);
+  fsm.add_transition(&state_pause,      &state_run,       EVENT_CONTINUE,       NULL);
+  fsm.add_transition(&state_config,     &state_idle,      EVENT_END_CONFIG,     NULL);
+  
   fsm.run_machine();
-  //Serial.println("Entering main loop.");
+
+  DEBUG Serial.println("Setup ready.");
+  DEBUG Serial.println("Entering main loop.");
+
 }
 
 void loop() {
   
-  // int hardStop = digitalRead(hardStopPin);
-  // if (hardStop == LOW) {
-  //   getPote();
-  //   if (Serial.available()) {
-  //     int velocidadSerial = Serial.parseInt();
-  //     setVelocidad(velocidadSerial);
-  //     setLeds();
-  //   }
-  // } else {
-  //   // HARD STOP
-  //   setVelocidad(0);
-  //   blinkLeds();
-  // }
-  // printData();
-  // delay(1);
-  // TODO: unificar recebir comandos y pasos en el loop
-  char command[STEP_STRING_SIZE + 1] = {0};
+  enum Events event_prev = EVENT_NULL;
+  int command = 0;
+
   if(event == EVENT_NULL){
+
     if(Serial.available()){
-      Serial.readBytesUntil('\n', command, STEP_STRING_SIZE);
-      //Serial.print("Recived command:");
-      //Serial.println(command);
-      switch (command[0]) {
+
+      command = Serial.read();
+
+      DEBUG Serial.print("Recived command:");
+      DEBUG Serial.println(char(command));
+
+      switch (command) {
         case 'a':
           event = EVENT_BEGIN_PROGRAM;
           break;
@@ -122,150 +129,235 @@ void loop() {
       };
     };
   };
+
   event_prev = event;
   fsm.trigger(event);
+  
   if(event != EVENT_NULL && event == event_prev){
-    //Serial.println("Command unattended.");
-    while(Serial.available()){Serial.read();};
+
+    DEBUG Serial.println("Command unattended.");
+    
+    while(Serial.available()) Serial.read();
+    
     event = EVENT_NULL;
+
   }
 }
 
+void on_state_hard_stop(){
+
+  DEBUG Serial.println("State_hard_stop.");
+
+  setVelocidad(0);
+  Serial.print("STOP");
+  DEBUG Serial.println(" event fired.");
+  delay(250); // Bouncing
+
+  while(digitalRead(hardStopPin) == HIGH){
+  
+    digitalWrite(ledPin, !digitalRead(ledPin));
+    delay(100);
+  
+  }  
+
+  event = EVENT_UNLOCK;
+
+  Serial.print("INIT");
+  DEBUG Serial.println(" event fired.");
+
+}
+
 void on_state_idle(){
-  //Serial.println("State_idle.");
+
+  DEBUG Serial.println("State_idle.");
   event = EVENT_NULL;
   setVelocidad(0);
   setLeds();
+
 }
 
 void on_state_getset(){
+
   char buffer[STEP_STRING_SIZE + 1] = {0};
   int steps = 0;
   int min = 0;
   int sec = 0;
   int vel = 0;
-  int i;
-  //Serial.println("State_getset.");
+
   event = EVENT_NULL;
+  
   memset(program, 0, sizeof(program));
-  //Serial.print("Enter step:");
-  //Serial.println(steps);
+  
+  DEBUG Serial.println("State_getset.");
+  
   while(steps < MAX_STEPS){
-    if(Serial.available()){
-      memset(buffer, '\0', sizeof(buffer));
-      Serial.readBytesUntil('\n', buffer, STEP_STRING_SIZE);
-      if(sscanf(buffer, "%d:%d#%d", &min, &sec, &vel) == 3){
-        program[steps].seconds = min * 60 + sec;
-        program[steps].velocity = vel; 
-        steps++;
-        //Serial.println("ACK");
-        //Serial.print("Reading ");
-        //Serial.println(buffer);
-        //Serial.print("Min: ");
-        //Serial.println(min);
-        //Serial.print("Sec: ");
-        //Serial.println(sec);
-        //Serial.print("Vel: ");
-        //Serial.println(vel);
-        //Serial.print("Enter step:");
-        //Serial.println(steps);
-      } else {
-        //Serial.print("Done reading after:");
-        //Serial.println(buffer);
+
+    DEBUG Serial.print("Enter step:");
+    DEBUG Serial.println(steps);
+  
+    while(Serial.available() < STEP_STRING_SIZE);
+  
+    memset(buffer, '\0', sizeof(buffer));
+    Serial.readBytes(buffer, STEP_STRING_SIZE);
+
+    if(sscanf(buffer, "%d:%d#%d", &min, &sec, &vel) == 3){
+
+      if(min == 0 && sec == 0 && vel == 0) {
+        DEBUG Serial.println("Endding program step recived.");
         break;
       }
-    }
-  };
-  if(steps){
-    event = EVENT_END_PROGRAM;
-  } else {
-    event = EVENT_DISCARD;
+
+      program[steps].seconds = min * 60 + sec;
+      program[steps].velocity = vel; 
+      steps++;
+
+      DEBUG Serial.println("ACK");
+      DEBUG Serial.print("Reading ");
+      DEBUG Serial.println(buffer);
+      DEBUG Serial.print("Min: ");
+      DEBUG Serial.println(min);
+      DEBUG Serial.print("Sec: ");
+      DEBUG Serial.println(sec);
+      DEBUG Serial.print("Vel: ");
+      DEBUG Serial.println(vel);
+
+    } else {
+
+      DEBUG Serial.print("Error reading:");
+      DEBUG Serial.println(buffer);
+
+      event = EVENT_ERROR;
+      break;
+
+    };
   };
 }
 
 void on_state_config(){
-  //Serial.println("State_config.");
+
+  DEBUG Serial.println("State_config.");
   sendPote();
+
 }
 
 void on_state_ready(){
-  //Serial.println("State_ready.");
+
+  DEBUG Serial.println("State_ready.");
   event = EVENT_NULL;
   current_step = 0;
   remain_time = 0;
   stop_time = 0;
   sendPote();
+
 }
 
 void on_state_run(){
+
   if(event != EVENT_NULL){
-    //Serial.println("State_run.");
+
+    DEBUG Serial.println("State_run.");
     event = EVENT_NULL;
+    digitalWrite(ledPin, HIGH);
+
   }
+
   while(true){
+
     if(current_step < MAX_STEPS && program[current_step].seconds != 0){
+
       if(remain_time != 0){
-        //Serial.print("Continue step:");
-        //Serial.println(current_step);
-        //Serial.print("Remain time (ms):");
-        //Serial.println(remain_time);
-        //Serial.print("Velocity:");
-        //Serial.println(program[current_step].velocity);
+
+        DEBUG Serial.print("Continue step:");
+        DEBUG Serial.println(current_step);
+        DEBUG Serial.print("Remain time (ms):");
+        DEBUG Serial.println(remain_time);
+        DEBUG Serial.print("Velocity:");
+        DEBUG Serial.println(program[current_step].velocity);
+
         stop_time = millis() + remain_time;
         remain_time = 0;
+
       } else if(stop_time == 0){
-        //Serial.print("Start step:");
-        //Serial.println(current_step);
-        //Serial.print("Duration (s):");
-        //Serial.println(program[current_step].seconds);
-        //Serial.print("Velocity:");
-        //Serial.println(program[current_step].velocity);
+
+        DEBUG Serial.print("Start step:");
+        DEBUG Serial.println(current_step);
+        DEBUG Serial.print("Duration (s):");
+        DEBUG Serial.println(program[current_step].seconds);
+        DEBUG Serial.print("Velocity:");
+        DEBUG Serial.println(program[current_step].velocity);
+        
         stop_time = millis() + program[current_step].seconds * 1000;
+
       };
+
       setVelocidad(program[current_step].velocity);
-      while(!Serial.available() && millis() < stop_time);
-      if(Serial.available()){
-        //Serial.println("Command was sent.");
+      setLeds();
+      
+      while(digitalRead(hardStopPin) != HIGH && !Serial.available() && millis() < stop_time);
+      
+      if(digitalRead(hardStopPin) == HIGH){
+
+        setVelocidad(0);
+        event = EVENT_HARD_STOP;
         break;
+
+      } else if(Serial.available()){
+
+        DEBUG Serial.println("Command was sent.");
+        break;
+
       } else {
-        //Serial.println("Step completed.");
+
+        Serial.print("STEP");
+        DEBUG Serial.println(" completed.");
         current_step++;
+        remain_time = 0;
+        stop_time = 0;
+
       }
+
     } else {
-      //Serial.println("Program completed.");
-      event = EVENT_COMPLETED;
+
       setVelocidad(0);
+      event = EVENT_COMPLETED;
+      DEBUG Serial.println("Program completed.");
+      digitalWrite(ledPin, LOW);
       break;
+
     }
   }
 }
 
 void on_state_pause(){
-  //Serial.println("State_pause.");
+
   unsigned long current_time = millis();
   event = EVENT_NULL;
+
+  DEBUG Serial.println("State_pause.");
+
   setVelocidad(0);
-  if(current_time < stop_time){
+  
+  if(current_time < stop_time)
     remain_time = stop_time - current_time;
-  } else {
-    current_step++;
-  };
+  else 
+    remain_time = 1;
+
 }
 
 void setVelocidad(int velocidadSerial) {
+
   if (velocidadSerial >= 0 && velocidadSerial <= 10) {
     velocidad = velocidadSerial;
     int velocidad_out = 255 * (float(velocidad) / 10);
     analogWrite(motorPin, velocidad_out);
   }
+
 }
 
 void setLeds() {
-  if (velocidad == 0) {
-    digitalWrite(ledPin, LOW);
-  } else {
-    digitalWrite(ledPin, HIGH);
-  }
+
+  digitalWrite(ledPin, (velocidad)? HIGH : LOW);
+
 }
 
 void blinkLeds() {
@@ -276,26 +368,42 @@ void blinkLeds() {
 }
 
 void getPote() {
+
   pote = analogRead(potePin);
+
 }
 
 void sendPote() {
+
   int prev_pote = pote;
+  
   event = EVENT_NULL;
+  
   while(!Serial.available()){
+  
     getPote();
+  
     if(pote != prev_pote){
+  
       Serial.println(pote);
       prev_pote = pote;
+      digitalWrite(ledPin, !digitalRead(ledPin));
       delay(500);
+
     }
+  
   }
+
+  digitalWrite(ledPin, LOW);
+
 }
 
 void printData() {
-  //Serial.print("Velocidad: ");
-  //Serial.print(velocidad);
-  //Serial.print(" - ");
-  //Serial.print("Peso: ");
-  //Serial.println(pote);
+
+  DEBUG Serial.print("Velocidad: ");
+  DEBUG Serial.print(velocidad);
+  DEBUG Serial.print(" - ");
+  DEBUG Serial.print("Peso: ");
+  DEBUG Serial.println(pote);
+
 }
